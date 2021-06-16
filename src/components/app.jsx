a46cb3f5f5ed9da29afb0719092b7d7b6696c3fe
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-06-17 01:49:01 +0600

    changes something in app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class App extends Component {
     render() {
-        return <h1> Hello, Welcome to my profile! I'm a professional front-end developer with more than 1 year of proven experience in Web Development. I can create beautiful designs into usable websites that respond to various device and user friendly. I have done a lot of local web developing projects, those are shown in my portfolio listed below</h1>
+        return <h1> Hello, Welcome to my profile! I'm a professional front-end developer with more than 1 year of proven experience in Web Development. I can create beautiful designs into usable websites that respond to various device and user friendly. I have done a lot of local web developing projects, those are shown in my portfolio listed below.I have great experience in JavaScript, React, HTML5, CSS3, Bootstrap , Jquery as my tools. I can build website front-ends focusing on performance, responsiveness/mobile-friendliness, usability and accessibility.I look forward to gather new experience and develop my skills.</h1>
     }
 }
 export default App;
