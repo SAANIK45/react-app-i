7022704a0acc071234c172d3eb87c6ad941d5d7f
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-06-17 01:43:08 +0600

    ceart app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class App extends Component {
     render() {
-        return <h1> Samiul Ahmed Anik adasdasdasd</h1>
+        return <h1> Hello, Welcome to my profile! I'm a professional front-end developer with more than 1 year of proven experience in Web Development. I can create beautiful designs into usable websites that respond to various device and user friendly. I have done a lot of local web developing projects, those are shown in my portfolio listed below</h1>
     }
 }
 export default App;
